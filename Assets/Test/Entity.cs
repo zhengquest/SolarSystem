0449b9c4988716961b9fb66e5ad4b7d1ed7e6e72
Zheng Qu <zhengqu53@hotmail.com>
2020-06-16 10:47:47 +0100

    render sun

--- Assets/Test/Entity.cs
+++ Assets/Test/Entity.cs
@@ -12,7 +12,9 @@ namespace Test
         public Vector2 Velocity { get; private set; }
 
         private OrbitEntityBehaviour m_behaviour;
-        
+        private float m_renderSize;
+        private Color m_renderColor;
+
         public Entity()
         {        
         }
@@ -32,6 +34,12 @@ namespace Test
             m_behaviour = behaviour;
         }
 
+        public void SetRenderParameter(float size, Color color)
+        {
+            m_renderSize = size;
+            m_renderColor = color;
+        }
+
         public void Update(float deltaTime)
         {
             m_behaviour?.Update(deltaTime);
@@ -48,14 +56,24 @@ namespace Test
             if (Velocity.magnitude < 0.01f)
             {
                 float size = 0.1f;
-                Vector3 up = new Vector3(0,1,0)*size;
-                Vector3 right = new Vector3(1,0,0)*size;
-                
-                Debug.DrawLine(pos + up + right, pos - up + right, Color.yellow);
-                Debug.DrawLine(pos - up + right, pos - up - right, Color.yellow);
-                Debug.DrawLine(pos - up - right, pos + up - right, Color.yellow);
-                Debug.DrawLine(pos + up - right, pos + up + right, Color.yellow);
-                
+                Vector3 up = new Vector3(0,1,0) * size;
+                Vector3 right = new Vector3(1,0,0) * size;
+
+                for (int i = 0, angle = 0; i < 5; i++, angle+= 25)
+                {
+                    Quaternion rot = Quaternion.AngleAxis(angle, Vector3.forward);
+                    Matrix4x4 matrix = Matrix4x4.TRS(Vector3.zero, rot, Vector3.one);
+                    
+                    Vector3 topLeft = matrix.MultiplyPoint3x4(up - right) + pos;
+                    Vector3 topRight = matrix.MultiplyPoint3x4(up + right) + pos;
+                    Vector3 botLeft = matrix.MultiplyPoint3x4(-up - right) + pos;
+                    Vector3 botRight = matrix.MultiplyPoint3x4(-up + right) + pos;
+                    
+                    Debug.DrawLine(topRight, botRight, Color.yellow);
+                    Debug.DrawLine(botRight, botLeft, Color.yellow);
+                    Debug.DrawLine(botLeft, topLeft, Color.yellow);
+                    Debug.DrawLine(topLeft, topRight, Color.yellow);
+                }
             }
             else
             {
@@ -63,16 +81,19 @@ namespace Test
                 // float size = 0.2f;
                 // Vector3 dirEnd = new Vector3(Position.x + direction.x*size, Position.y + direction.y*size, 0);
                 // Debug.DrawLine(pos, dirEnd, Color.white);
-                float size = 0.1f;
+                
+                float size = 0.05f;
                 Vector3 nextPoint = pos;
                 Vector3 startPoint = pos + new Vector3(Mathf.Cos(0), Mathf.Sin(0), 0) * size;
             
-                for(var i = 1; i < 91; i++)
+                for(int i = 1; i < 91; i++)
                 {
                     nextPoint.x = Mathf.Cos((i*4)*Mathf.Deg2Rad) * size;
                     nextPoint.y = Mathf.Sin((i*4)*Mathf.Deg2Rad) * size;
                     nextPoint += pos;
+                    
                     Debug.DrawLine(startPoint, nextPoint, Color.yellow);
+                    
                     startPoint = nextPoint;
                 }
             
