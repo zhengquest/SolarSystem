8175fa009529a3d74767b6cdab966ce4a3b2c5e3
Zheng Qu <zhengqu53@hotmail.com>
2020-06-16 09:15:24 +0100

    draw circle

--- Assets/Test/Entity.cs
+++ Assets/Test/Entity.cs
@@ -45,8 +45,8 @@ namespace Test
             Vector3 pos = new Vector3(Position.x, Position.y, 0);
             
             // We assume this is stationary body (planet) if its velocity is too low.
-            // if (Velocity.magnitude < 0.01f)
-            // {
+            if (Velocity.magnitude < 0.01f)
+            {
                 float size = 0.1f;
                 Vector3 up = new Vector3(0,1,0)*size;
                 Vector3 right = new Vector3(1,0,0)*size;
@@ -55,17 +55,30 @@ namespace Test
                 Debug.DrawLine(pos - up + right, pos - up - right, Color.yellow);
                 Debug.DrawLine(pos - up - right, pos + up - right, Color.yellow);
                 Debug.DrawLine(pos + up - right, pos + up + right, Color.yellow);
-            // }
-            // else
-            // {
-            //     Vector2 direction = Velocity.normalized;
-            //     float size = 0.2f;
-            //     Vector3 dirEnd = new Vector3(Position.x + direction.x*size, Position.y + direction.y*size, 0);
-            //     Debug.DrawLine(pos, dirEnd, Color.white);
                 
+            }
+            else
+            {
+                // Vector2 direction = Velocity.normalized;
+                // float size = 0.2f;
+                // Vector3 dirEnd = new Vector3(Position.x + direction.x*size, Position.y + direction.y*size, 0);
+                // Debug.DrawLine(pos, dirEnd, Color.white);
+                float size = 0.1f;
+                Vector3 nextPoint = pos;
+                Vector3 startPoint = pos + new Vector3(Mathf.Cos(0), Mathf.Sin(0), 0) * size;
+            
+                for(var i = 1; i < 91; i++)
+                {
+                    nextPoint.x = Mathf.Cos((i*4)*Mathf.Deg2Rad) * size;
+                    nextPoint.y = Mathf.Sin((i*4)*Mathf.Deg2Rad) * size;
+                    nextPoint += pos;
+                    Debug.DrawLine(startPoint, nextPoint, Color.yellow);
+                    startPoint = nextPoint;
+                }
+            
+            
                 m_behaviour?.Render();
-            // }
+            }
         }
-
     }
 }
