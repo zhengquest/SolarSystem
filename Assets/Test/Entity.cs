2d6c2cee6228b3bc15a12fd91bf09cf10a587ab5
Zheng Qu <zhengqu53@hotmail.com>
2020-06-16 10:58:34 +0100

    make sun pretty

--- Assets/Test/Entity.cs
+++ Assets/Test/Entity.cs
@@ -59,15 +59,16 @@ namespace Test
                 Vector3 up = new Vector3(0,1,0) * size;
                 Vector3 right = new Vector3(1,0,0) * size;
 
-                for (int i = 0, angle = 0; i < 5; i++, angle+= 25)
+                for (int i = 0, angle = 0; i < 10; i++, angle+= 45)
                 {
                     Quaternion rot = Quaternion.AngleAxis(angle, Vector3.forward);
-                    Matrix4x4 matrix = Matrix4x4.TRS(Vector3.zero, rot, Vector3.one);
+                    float newSize = size * i;
+                    Matrix4x4 matrix = Matrix4x4.TRS(pos, rot, new Vector3(newSize,newSize,newSize));
                     
-                    Vector3 topLeft = matrix.MultiplyPoint3x4(up - right) + pos;
-                    Vector3 topRight = matrix.MultiplyPoint3x4(up + right) + pos;
-                    Vector3 botLeft = matrix.MultiplyPoint3x4(-up - right) + pos;
-                    Vector3 botRight = matrix.MultiplyPoint3x4(-up + right) + pos;
+                    Vector3 topLeft = matrix.MultiplyPoint3x4(up - right);
+                    Vector3 topRight = matrix.MultiplyPoint3x4(up + right);
+                    Vector3 botLeft = matrix.MultiplyPoint3x4(-up - right);
+                    Vector3 botRight = matrix.MultiplyPoint3x4(-up + right);
                     
                     Debug.DrawLine(topRight, botRight, Color.yellow);
                     Debug.DrawLine(botRight, botLeft, Color.yellow);
