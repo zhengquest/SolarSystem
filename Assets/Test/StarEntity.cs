ad955ab95035524161d88e00db073112028b7647
Zheng Qu <zhengqu53@hotmail.com>
2020-06-16 12:34:37 +0100

    give sun a bit of spin

--- Assets/Test/StarEntity.cs
+++ Assets/Test/StarEntity.cs
@@ -10,14 +10,14 @@ namespace Test
 
 		public override void Render()
 		{
-			float size = 0.1f;
+			float size = 0.08f;
 			Vector3 pos = new Vector3(Position.x, Position.y, 0);
 			Vector3 up = new Vector3(0,1,0) * size;
 			Vector3 right = new Vector3(1,0,0) * size;
 
-			for (int i = 0, angle = 0; i < 10; i++, angle+= 45)
+			for (int i = 0, angle = 0; i < 20; i++, angle+= 33)
 			{
-				Quaternion rot = Quaternion.AngleAxis(angle, Vector3.forward);
+				Quaternion rot = Quaternion.AngleAxis(angle + Time.time * 100f, Vector3.forward);
 				float newSize = size * i;
 				Matrix4x4 matrix = Matrix4x4.TRS(pos, rot, new Vector3(newSize,newSize,newSize));
                     
